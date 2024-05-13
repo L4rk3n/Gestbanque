3d93c449bd16be2498be49a4920b9ff13da68cb6
L4rk3n <cedric.bounidas@gmail.com>
2024-05-13 15:12:11 +0200

    Ajoutez des fichiers projet.

---
+++ Gestbanque/Program.cs
@@ -0,0 +1,30 @@
+﻿using Models;
+
+Personne p1= new Personne();
+p1.Nom = "Bouvier";
+p1.Prenom = "Cédric";
+p1.Datenaiss = new DateTime(1980, 12, 22);
+
+Personne p2 = new Personne()
+{
+    Nom = "Morre",
+    Prenom = "Thierry",
+    Datenaiss = new DateTime(1970, 1, 1)
+};
+
+Console.WriteLine($"identité {p1.Nom} {p1.Prenom}, né le {p1.Datenaiss}");
+Console.WriteLine($"identité {p2.Nom} {p2.Prenom}, né le {p2.Datenaiss}");
+
+Courant c1 = new Courant()
+{
+    Numero = "BE01",
+    LigneDeCredit = 200,
+    Titulaire = p1
+};
+
+c1.Depot(20_000);
+Console.WriteLine($"Le compte courant : {c1.Numero} du titulaire : {c1.Titulaire.Nom} {c1.Titulaire.Prenom}");
+Console.WriteLine($"Né le  {c1.Titulaire.Datenaiss}");
+Console.WriteLine($"Le solde est de : {c1.Solde} et la ligne de crédit de : {c1.LigneDeCredit}");
+
+c1.Retrait(21000);
---
+++ Models/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Models {
+
+    public class Personne
+    {
+        public string Nom { get; set;}
+        public string Prenom { get; set;}
+        public DateTime Datenaiss { get; set;}
+    }
+
+    public class Courant
+    {
+        private double _ldc;
+        public string Numero { get; set;}
+        public Personne Titulaire { get; set;}  
+        public double Solde { get; private set; }
+        public double LigneDeCredit
+        {
+            get
+            {
+                return _ldc;
+            }
+            set
+            {
+                if (value >= 0)
+                    { 
+                        _ldc = value;
+                    }
+            }
+        }
+        public void Retrait(double Montant)
+        {
+            if (Montant<=0) return;
+            if (Montant > (Solde + LigneDeCredit)) return;
+            Solde -= Montant;
+        }
+        public void Depot(double Montant)
+        {
+            if (Montant <= 0) return; //return va a l accolade fermante sans executer le reste des lignes.
+            Solde += Montant;
+        }
+    }
+}
